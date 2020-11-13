387658d22637e5927c805b26e33cc1b9b24d9b6e
Marné <vanzyl.marne007@gmail.com>
2020-11-13 17:43:51 +0200

    New implimentation of MERN

--- backend/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const connection = "mongodb+srv://vanzyl.marne007@gmail.com:<Awesomebees>@<popiact-data.63sbu.mongodb.net>/<PopiAct-Solution>?retryWrites=true&w=majority";
-//mongo "mongodb+srv://popiact-data.63sbu.mongodb.net/<dbname>" --username marnevanzyl
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
