35a019304034087265de24e27c1f3446111cd650
Marné <vanzyl.marne007@gmail.com>
2020-11-14 18:18:09 +0200

    Connected Frontend With API'S to make requests from DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import Information from './components/Information';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -13,7 +15,10 @@ function App() {
     <Provider store={store}>
     <div className="App">
       <AppNavbar/>
+      <Container>
+      <ItemModal/>
       <Information/>
+      </Container>
     </div>
     </Provider>
   );
--- client/src/components/Information.js
+++ client/src/components/Information.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-//import uuid from 'uuid';
-import { v4 as uuid } from 'uuid';
 import { connect } from 'react-redux';
-import { getItems } from '../actions/itemActions';
+import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class Information extends Component {
@@ -13,41 +11,26 @@ class Information extends Component {
         this.props.getItems();
     }
 
+    onDeleteClick = (id) => {
+        this.props.deleteItem(id);
+    }
+
     render() {
         const { items } = this.props.item;
         return(
             <Container>
-                <Button
-                    className="add-btn"
-                    color="dark"
-                    style={{marginBotton: '2rem'}}
-                    onClick={() => {
-                        const name = prompt('Enter Item');
-                        if(name){
-                            this.setState(state => ({
-                                items: [...state.items, {id: uuid(), name}]
-                            }));
-                        }
-                    }}
-                >
-                    Add Item
-                </Button>
-
                 <ListGroup>
                     <TransitionGroup className="information-list">
-                        {items.map(({id, name}) => (
-                            <CSSTransition key={id} timeout={500} classNames="fade">
+                        {items.map(({_id, name}) => (
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
                                     <Button
                                     className="remove-btn"   
                                     color="danger"
                                     size="sm"
-                                    onClick={() => {
-                                        this.setState(state => ({
-                                            items: state.items.filter(item => item.id !== id)
-                                        }));
-                                    }}
-                                    >&times;
+                                    onClick={this.onDeleteClick.bind(this, _id)}
+                                    >
+                                    &times;
                                     </Button>
                                     {name}
                                 </ListGroupItem>
@@ -70,4 +53,7 @@ const mapStateToProps = (state) => ({
     item: state.item
 });
 
-export default connect(mapStateToProps, { getItems })(Information);
+export default connect(
+    mapStateToProps, 
+    { getItems, deleteItem }
+    )(Information);
