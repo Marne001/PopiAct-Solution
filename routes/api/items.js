7e7bbb73d4db6cbebdaece0278d6a06cd1bb9370
Marné <vanzyl.marne007@gmail.com>
2020-11-14 20:22:58 +0200

    Deployed on Heroku, Added Users and JWT Auth

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,6 +1,7 @@
 //const { request } = require('express');
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 // Item Model
 const Item = require('../../models/Item');
@@ -16,8 +17,8 @@ router.get('/', (req, res) => {
 
 // @route POST api/items
 // @desc Create a Item
-// @access Public
-router.post('/', (req, res) => {
+// @access Private
+router.post('/', auth, (req, res) => {
     const newItem = new Item({
         name: req.body.name
     });
@@ -27,8 +28,8 @@ router.post('/', (req, res) => {
 
 // @route DELETE api/items/:id
 // @desc Delete a Item
-// @access Public
-router.delete('/:id', (req, res) => {
+// @access Private
+router.delete('/:id', auth, (req, res) => {
     Item.findById(req.params.id)
         .then(item => item.remove().then(() => res.json({success: true})))
         .catch(err => res.status(404).json({success: false}));
--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
+const config = require('config');
 
-const items = require('./routes/api/items');
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI
+const db = config.get('mongoURI');
 
 //Connect to Mongo
 mongoose
-.connect(db)
+.connect(db, {useNewUrlParser: true, useCreateIndex: true}) //adding new mongo url parser
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 mongoose.connect(db, {useNewUrlParser: true})
 
 //Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
